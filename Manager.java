99347487a3b8b8eb6d483fea22c1b73fca78ec5c
PhanTruong12 <122134064+PhanTruong12@users.noreply.github.com>
2023-03-10 09:47:15 +0700

    Update Manager.java
    Signed-off-by: PhanTruong12 <122134064+PhanTruong12@users.noreply.github.com>

--- Manager.java
+++ Manager.java
@@ -1,6 +1,47 @@
 
 package controller;
+import java.util.*;
 
-public class Manager {
-    
+    public class Manager {
+        public void sortEmployees() {
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                int lastNameCompare = o1.getFullName().split(" ")[1].compareTo(o2.getFullName().split(" ")[1]);
+                if (lastNameCompare == 0) {
+                    return Double.compare(o1.getSalary(), o2.getSalary());
+                }
+                return lastNameCompare;
+            }
+        });
+        System.out.println("Employees have been sorted.");
+    }
+
+    public void findSalaryRange(double minSalary, double maxSalary) {
+        for (Employee employee : employees) {
+            if (employee.getSalary() >= minSalary && employee.getSalary() <= maxSalary) {
+                System.out.println(employee.getFullName() + " - " + employee.getSalary());
+            }
+        }
+    }
+    public void displayRetiredEmployees() {
+        List<Employee> retiredEmployees = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getAge() >= 65) {
+                retiredEmployees.add(employee);
+            }
+        }
+        Collections.sort(retiredEmployees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return Double.compare(o2.getSalary(), o1.getSalary());
+            }
+        });
+        System.out.println("The top 3 highest paid retired employees are:");
+        for (int i = 0; i < Math.min(3, retiredEmployees.size()); i++) {
+            Employee employee = retiredEmployees.get(i);
+            System.out.println(employee.getFullName() + " - " + employee.getSalary());
+        }
+    }
+
 }
