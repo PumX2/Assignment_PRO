74488e7616739fb0cc6ee5d2894e12b61fb158db
PhanTruong12 <122134064+PhanTruong12@users.noreply.github.com>
2023-03-10 14:13:34 +0700

    Update Manager.java
    Signed-off-by: PhanTruong12 <122134064+PhanTruong12@users.noreply.github.com>

--- Manager.java
+++ Manager.java
@@ -9,7 +9,7 @@ public class Manager {
     public void findSalaryRange(double minSalary, double maxSalary) {
     for (Employee employee : employees) {
         if (employee.getSalary() >= minSalary && employee.getSalary() <= maxSalary) {
-            System.out.println(employee.getFullName() + " - " + employee.getSalary());
+            System.out.println(employee.getName() + " - " + employee.getSalary());
         }
     }
 }
@@ -17,7 +17,7 @@ public class Manager {
     Collections.sort(employees, new Comparator<Employee>() {
         @Override
         public int compare(Employee o1, Employee o2) {
-            int lastNameCompare = o1.getFullName().split(" ")[1].compareTo(o2.getFullName().split(" ")[1]);
+            int lastNameCompare = o1.getName().split(" ")[1].compareTo(o2.getName().split(" ")[1]);
             if (lastNameCompare == 0) {
                 return Double.compare(o1.getSalary(), o2.getSalary());
             }
@@ -42,7 +42,7 @@ public class Manager {
     System.out.println("The top 3 highest paid retired employees are:");
     for (int i = 0; i < Math.min(3, retiredEmployees.size()); i++) {
         Employee employee = retiredEmployees.get(i);
-        System.out.println(employee.getFullName() + " - " + employee.getSalary());
+        System.out.println(employee.getName() + " - " + employee.getSalary());
     }
 }
 
